91eddd0c8b57ea1b5922ea2a060fdef68d310f82
Jawwad Iqbal <jawwad.iqbal@doozielabs.com>
2019-09-26 15:49:32 +0500

    --Open CLose Principle

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const Person = require('./SingleResponsiblityPrinciple/person')
-
 //Single Responsibilty Principle
+const Person = require('./SingleResponsiblityPrinciple/person')
 var Jawwad = new Person('Jawwad', 'Test', "testing123.com");
-console.log(Jawwad)
+// console.log(Jawwad)
+
+//Open Close Principle
+const Rectangle = require('./OpenClosePrinciple/Rectangle')
+var myShape = new Rectangle(2, 9);
+console.log(myShape.area)
